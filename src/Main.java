4b0671fb98ef70b337032a6bdda681e8c5fa7c4d
ann_daniels <anna.danielyan01@gmail.com>
2022-05-06 16:35:54 +0400

    Ex 1 modified

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        Spring s = new Spring();
-        double[] t = s.move(1,0.1,2,2);
-        for (int i = 0; i < t.length; i++)
-        {
-            System.out.println(t[i]);
-        }
+//        Spring s = new Spring();
+//        double[] t = s.move(1.0, 0.1, 2.0, 2.0);
+//        double[] t = s.move(1.0, 0.1, 2.0);
+//        double[] t = s.move(0, 4.0, 0.2, 0.1, 2.0);
+//        double[] t = s.move(0, 4.0, 0.2, 0.1, 2.0, 2.0);
+//
+//        for (int i = 0; i < t.length; i++)
+//        {
+//            System.out.println(t[i]);
+//        }
+
+        
+
     }
 }
--- src/Spring.java
+++ src/Spring.java
@@ -24,19 +24,56 @@ public class Spring
         this.k = k;
     }
 
-    public double[] move(double t, double dt, double x0, double v0)
-    {
-        return move(0,t,dt,x0,v0,1.0);
-    }
-
     public double[] move(double t, double dt, double x0)
     {
-        return move(0,t,dt,x0,0,1.0);
+        int size = (int)(t/dt);
+        double[] coordinates = new double[size+1];
+        double omega = sqrt(k);
+        int index = 0;
+
+        for (double i = 0; i <= t ; i += dt)
+        {
+            double xi = (x0 * cos(omega*i));
+            coordinates[index] = xi;
+            index++;
+        }
+
+        return coordinates;
+
+    }
+    public double[] move(double t, double dt, double x0, double v0)
+    {
+        int size = (int)(t/dt);
+        double[] coordinates = new double[size+1];
+        double omega = sqrt(k);
+        int index = 0;
+
+        for (double i = 0; i <= t ; i += dt)
+        {
+            double xi = (x0 * cos(omega*i)) + (v0/omega * sin(omega*i));
+            coordinates[index] = xi;
+            index++;
+        }
+
+        return coordinates;
+
     }
 
     public double[] move(double t0, double t1, double dt, double x0, double v0)
     {
-       return move(t0,t1,dt,x0,v0,1.0);
+        int size = (int)((t1-t0)/dt);
+        double[] coordinates = new double[size+1];
+        double omega = sqrt(k);
+        int index = 0;
+
+        for (double t = t0; t <= t1 ; t += dt)
+        {
+            double xt = (x0 * cos(omega*t)) + (v0/omega * sin(omega*t));
+            coordinates[index] = xt;
+            index++;
+        }
+
+        return coordinates;
     }
 
     public double[] move(double t0, double t1, double dt, double x0, double v0, double m)
